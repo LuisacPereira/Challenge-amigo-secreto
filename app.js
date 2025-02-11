0964fde5b74b840291a6c2f58f22befdf00581cb
Luisa Pereira <pereiracluisa@gmail.com>
2025-02-11 00:06:51 -0300

    atualizar lista amigos na tela

--- app.js
+++ app.js
@@ -2,16 +2,31 @@ let amigos = [];
 
 function adicionarAmigo () {
     let nome = document.querySelector('#amigo').value;
-    if(nome == null) {
+    if(nome == '') {
         alert('Por favor, insira um nome');
     } else {
         amigos.push(nome);
     }
-
     limparCampo();
+    adicionarAmigosNaLista();
 }
+
 
 function limparCampo () {
     nome = document.querySelector('#amigo');
     nome.value = ' ';
+}
+
+function adicionarAmigosNaLista() {
+    const lista = document.querySelector('#listaAmigos');
+    
+
+    lista.innerHTML = "";
+    
+    for (let x = 0; x < amigos.length; x++) {
+        let li = document.createElement('li');
+        li.textContent = amigos[x];
+        lista.appendChild(li);
+    }
+
 }

96b5d3e4559dc9d180a65f2c80fcb19dfde50445
Luisa Pereira <pereiracluisa@gmail.com>
2025-02-11 01:01:09 -0300

    sorteio do amigo

--- app.js
+++ app.js
@@ -18,15 +18,26 @@ function limparCampo () {
 }
 
 function adicionarAmigosNaLista() {
-    const lista = document.querySelector('#listaAmigos');
-    
-
-    lista.innerHTML = "";
-    
+    let lista = document.querySelector('#listaAmigos');
+    lista.innerHTML = '';
     for (let x = 0; x < amigos.length; x++) {
         let li = document.createElement('li');
         li.textContent = amigos[x];
         lista.appendChild(li);
     }
+}
 
+function sortearAmigo() {
+    let qtdAmigos = amigos.length;
+    let amigoEscolhido;
+    if (qtdAmigos > 0) {
+        let escolhido = Math.floor(Math.random() * qtdAmigos);
+        amigoEscolhido = amigos[escolhido];
+        let resultado = document.getElementById('resultado');
+        resultado.innerHTML = `O amigo secreto é ${amigoEscolhido}`; 
+        lista = document.querySelector('#listaAmigos');
+        lista.innerHTML = '';
+    } else {
+        alert('Impossível sortear, insira um nome na lista');
+    } 
 }

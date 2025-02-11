1890536f6af8cea2b2ec0e29281f088c40af03e2
Luisa Pereira <pereiracluisa@gmail.com>
2025-02-10 23:00:28 -0300

    criação função adicionarAmigo

--- app.js
+++ app.js
@@ -1,1 +1,17 @@
-//O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
+let amigos = [];
+
+function adicionarAmigo () {
+    let nome = document.querySelector('#amigo').value;
+    if(nome == null) {
+        alert('Por favor, insira um nome');
+    } else {
+        amigos.push(nome);
+    }
+
+    limparCampo();
+}
+
+function limparCampo () {
+    nome = document.querySelector('#amigo');
+    nome.value = ' ';
+}
